062b631a19c3ffb9384621861bdd4e028a4d32f5
weelam <wl.lin@mail.utoronto.ca>
2021-11-16 03:05:32 -0500

    Added a menu option for the widgets. Allows user to remove the widget.

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 const widgetStyle = `
 	animation: begindrag 0.8s ease forwards; 
 	opacity: 0.5;
-	z-index: 999999999999999999;
-	box-shadow: 0 0 5px #ddd;
+	z-index: 10;
+	box-shadow: 0 0 3px black;
 	`;
 
 const resizerStyle = `
 	position: absolute;
 	background-color: transparent;
-	z-index: 3;
+	z-index: 999999999999999;
 	width: 15px;
 	height: 15px;
 `;
@@ -18,28 +18,38 @@ const resizerStyle_nw = `
 	cursor: nw-resize;
 	top: 0px;
 	left: 0px;
-`
+`;
 
 const resizerStyle_ne = `
 	cursor: ne-resize;
 	top: 0px;
 	right: 0px;
-`
+`;
 
-const resizerStyle_sw  = `
+const resizerStyle_sw = `
 	cursor: sw-resize;
 	bottom: 0px;
 	left: 0px;
 
-`
+`;
 
 const resizerStyle_se = `
 	cursor: se-resize;
 	bottom: 0px;
 	right: 0px;
 
-`
-
+`;
+const taskbarStylePositioner = `
+	position: absolute;
+	height: 10px;
+	width: 10px;
+	z-index: 99999999999;
+`;
+const taskbarStyle = `
+	top: 0px;
+	right: -8px;
+	background: black;
+`;
 const widgets = document.querySelectorAll(".widgit-widget");
 let resizing = false;
 const resizes = ["widgit-se", "widgit-sw", "widgit-ne", "widgit-nw"];
@@ -66,15 +76,17 @@ const handleMouseDown = (e, widget) => {
         console.log("no new clone");
       }
 
-			const wrapper = widget.parentNode
-			console.log(wrapper)
+      const wrapper = widget.parentNode;
+      console.log(wrapper);
       addResizer(widget);
-      // add class
-      // widget.classList.add("widgit-dragging");
-      widget.style.cssText += widgetStyle;			
+      openTaskBar(wrapper, true);
+      // add widget styles
+      widget.style.cssText += widgetStyle;
       widget.setAttribute("draggable", false);
       widget.style.cursor = "move";
-
+      // add wrapper styles
+      wrapper.style.height = widget.clientHeight + "px";
+      wrapper.style.width = widget.clientWidth + "px";
       // this only happens when we mouse down
       let x1 = e.clientX;
       let y1 = e.clientY;
@@ -85,9 +97,9 @@ const handleMouseDown = (e, widget) => {
         let y2 = e.clientY - y1;
 
         // console.log("x2", x2, "y2", y2);
-				
-				// we must change the positioning of the widget relative to the wrapper's positioning
-				// this is because the widget itself will mess with the positioning when dragging for some unknown reason
+
+        // we must change the positioning of the widget relative to the wrapper's positioning
+        // this is because the widget itself will mess with the positioning when dragging for some unknown reason
         const bounding = wrapper.getBoundingClientRect();
 
         // rather than reassigning the position to your cursor location, we are updating the position properties instead
@@ -95,8 +107,8 @@ const handleMouseDown = (e, widget) => {
         // when you start dragging.
         widget.style.left = bounding.left + x2 + "px";
         widget.style.top = bounding.top + y2 + "px";
-				// must also update the wrapper's location in the same why since we're taking the boundClientRect of the wrpaper
-				wrapper.style.left = bounding.left + x2 + "px";
+        // must also update the wrapper's location in the same why since we're taking the boundClientRect of the wrpaper
+        wrapper.style.left = bounding.left + x2 + "px";
         wrapper.style.top = bounding.top + y2 + "px";
         let data = {
           x2: x2,
@@ -126,6 +138,24 @@ const handleMouseDown = (e, widget) => {
   }
 };
 
+// helper functions
+const openTaskBar = (wrapper, open) => {
+  if (open) {
+    const taskbar = document.createElement("div");
+		wrapper.append(taskbar);
+    taskbar.style.cssText += taskbarStylePositioner + taskbarStyle;
+
+    const removeWidget = document.createElement("button");
+    removeWidget.innerHTML = "Remove";
+    removeWidget.onclick = () => wrapper.remove();
+    taskbar.append(removeWidget);
+
+		const closeButton = document.createElement("button");
+		closeButton.innerHTML = "Close";
+    closeButton.onclick = () => taskbar.remove();
+    taskbar.append(closeButton);
+  }
+};
 const cloneWidget = (widget) => {
   // clone node
   const parentNode = widget.parentNode;
@@ -134,12 +164,12 @@ const cloneWidget = (widget) => {
   widgetClone.classList.add("box");
   widgetClone.setAttribute("clone", true);
   parentNode.append(widgetClone);
-	
-	// wrap clone inside div
-	const clonedParentNode = widgetClone.parentNode;
-	const wrapper = document.createElement("div");
-	wrapper.appendChild(widgetClone);
-	clonedParentNode.appendChild(wrapper)
+
+  // wrap clone inside div
+  const clonedParentNode = widgetClone.parentNode;
+  const wrapper = document.createElement("div");
+  wrapper.appendChild(widgetClone);
+  clonedParentNode.appendChild(wrapper);
 
   widgetClone.addEventListener("mousedown", (e) =>
     handleMouseDown(e, widgetClone)
@@ -148,22 +178,22 @@ const cloneWidget = (widget) => {
   // reset widget timeout
   widgetClone.addEventListener("mouseup", () => resetTimeout(timeout_id));
   widgetClone.addEventListener("mouseleave", () => resetTimeout(timeout_id));
-	// widgetClone.addEventListener("click", () => widgetClone.remove())
+  // widgetClone.addEventListener("click", () => widgetClone.remove())
   const bounding = widget.getBoundingClientRect();
 
-	// must change wrapper's position in the same way since we'll be moving the element relative to the wrapper
-	// and not the widget itself
+  // must change wrapper's position in the same way since we'll be moving the element relative to the wrapper
+  // and not the widget itself
   widgetClone.style.position = "fixed";
-	wrapper.style.position = "fixed";
+  wrapper.style.position = "fixed";
   widgetClone.style.top = bounding.top + "px";
   widgetClone.style.left = bounding.left + "px";
-	wrapper.style.top = bounding.top + "px";
+  wrapper.style.top = bounding.top + "px";
   wrapper.style.left = bounding.left + "px";
   return widgetClone;
 };
 
 const handleResizer = (widget, resizeNodes) => {
-	const wrapper = widget.parentNode
+  const wrapper = widget.parentNode;
   resizeNodes.forEach((node) => {
     const handleResizeMouseDown = (e) => {
       currentResizer = e.target;
@@ -173,29 +203,29 @@ const handleResizer = (widget, resizeNodes) => {
       let y1 = e.clientY;
 
       const handleResizeMouseMove = (e) => {
-        const bounding = widget.getBoundingClientRect();
+        const bounding = wrapper.getBoundingClientRect();
         const x2 = e.clientX - x1;
         const y2 = e.clientY - y1;
 
-				// before we introduced the idea of positioning relative to a wrapper, resizing was weird
-				// now that we have a wrapper, it's good (must also update the width and height of the wrapper the same way as widget too)
+        // before we introduced the idea of positioning relative to a wrapper, resizing was weird
+        // now that we have a wrapper, it's good (must also update the width and height of the wrapper the same way as widget too)
         if (currentResizer.getAttribute("se")) {
           widget.style.width = bounding.width + x2 + "px";
           widget.style.height = bounding.height + y2 + "px";
-					wrapper.style.width = bounding.width + x2 + "px";
+          wrapper.style.width = bounding.width + x2 + "px";
           wrapper.style.height = bounding.height + y2 + "px";
         } else if (currentResizer.getAttribute("sw")) {
           widget.style.width = bounding.width - x2 + "px";
           widget.style.height = bounding.height + y2 + "px";
           widget.style.left = bounding.left + x2 + "px";
-					wrapper.style.width = bounding.width - x2 + "px";
+          wrapper.style.width = bounding.width - x2 + "px";
           wrapper.style.height = bounding.height + y2 + "px";
           wrapper.style.left = bounding.left + x2 + "px";
         } else if (currentResizer.getAttribute("ne")) {
           widget.style.width = bounding.width + x2 + "px";
           widget.style.height = bounding.height - y2 + "px";
           widget.style.top = bounding.top + y2 + "px";
-					wrapper.style.width = bounding.width + x2 + "px";
+          wrapper.style.width = bounding.width + x2 + "px";
           wrapper.style.height = bounding.height - y2 + "px";
           wrapper.style.top = bounding.top + y2 + "px";
         } else {
@@ -203,7 +233,7 @@ const handleResizer = (widget, resizeNodes) => {
           widget.style.height = bounding.height - y2 + "px";
           widget.style.top = bounding.top + y2 + "px";
           widget.style.left = bounding.left + x2 + "px";
-					wrapper.style.width = bounding.width - x2 + "px";
+          wrapper.style.width = bounding.width - x2 + "px";
           wrapper.style.height = bounding.height - y2 + "px";
           wrapper.style.top = bounding.top + y2 + "px";
           wrapper.style.left = bounding.left + x2 + "px";
@@ -227,22 +257,23 @@ const handleResizer = (widget, resizeNodes) => {
 };
 
 const addResizer = (widget) => {
+  const wrapper = widget.parentNode;
   const nw = document.createElement("div");
   const ne = document.createElement("div");
   const sw = document.createElement("div");
   const se = document.createElement("div");
 
-	nw.setAttribute("nw", true)
-	ne.setAttribute("ne", true)
-	sw.setAttribute("sw", true)
-	se.setAttribute("se", true)
+  nw.setAttribute("nw", true);
+  ne.setAttribute("ne", true);
+  sw.setAttribute("sw", true);
+  se.setAttribute("se", true);
 
   nw.style.cssText += resizerStyle_nw + resizerStyle;
   ne.style.cssText += resizerStyle_ne + resizerStyle;
   sw.style.cssText += resizerStyle_sw + resizerStyle;
   se.style.cssText += resizerStyle_se + resizerStyle;
 
-  widget.append(nw, ne, sw, se);
+  wrapper.append(nw, ne, sw, se);
   handleResizer(widget, [nw, ne, sw, se]);
 };
 
