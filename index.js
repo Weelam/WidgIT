6f7b6f1e0d7bc822224e08db0c0a7b102df5ddc6
weelam <wl.lin@mail.utoronto.ca>
2021-11-16 02:22:02 -0500

    Widget positioning is finally fixed by wrapper widgets inside a div. Resizing is consequently also fixed.

--- index.js
+++ index.js
@@ -1,9 +1,50 @@
+// inline styles
+const widgetStyle = `
+	animation: begindrag 0.8s ease forwards; 
+	opacity: 0.5;
+	z-index: 999999999999999999;
+	box-shadow: 0 0 5px #ddd;
+	`;
+
+const resizerStyle = `
+	position: absolute;
+	background-color: transparent;
+	z-index: 3;
+	width: 15px;
+	height: 15px;
+`;
+
+const resizerStyle_nw = `
+	cursor: nw-resize;
+	top: 0px;
+	left: 0px;
+`
+
+const resizerStyle_ne = `
+	cursor: ne-resize;
+	top: 0px;
+	right: 0px;
+`
+
+const resizerStyle_sw  = `
+	cursor: sw-resize;
+	bottom: 0px;
+	left: 0px;
+
+`
+
+const resizerStyle_se = `
+	cursor: se-resize;
+	bottom: 0px;
+	right: 0px;
+
+`
+
 const widgets = document.querySelectorAll(".widgit-widget");
 let resizing = false;
 const resizes = ["widgit-se", "widgit-sw", "widgit-ne", "widgit-nw"];
 let timeout_id;
 let clonedWidgets = [];
-
 
 widgets.forEach((widget) => {
   // when mouse is down
@@ -17,22 +58,21 @@ widgets.forEach((widget) => {
 const handleMouseDown = (e, widget) => {
   if (!resizing) {
     timeout_id = setTimeout(() => {
-			console.log(widget.getAttribute("clone"))
+      console.log(widget.getAttribute("clone"));
       if (!widget.getAttribute("clone")) {
         widget = cloneWidget(widget);
-				console.log("new clone")
+        console.log("new clone");
       } else {
-				console.log("no new clone")
-			}
+        console.log("no new clone");
+      }
 
+			const wrapper = widget.parentNode
+			console.log(wrapper)
       addResizer(widget);
       // add class
       // widget.classList.add("widgit-dragging");
-			widget.style.position = "fixed";
-      widget.style.cssText +=
-        "animation: begindrag 0.8s ease forwards; opacity: 0.5";
+      widget.style.cssText += widgetStyle;			
       widget.setAttribute("draggable", false);
-      
       widget.style.cursor = "move";
 
       // this only happens when we mouse down
@@ -45,14 +85,19 @@ const handleMouseDown = (e, widget) => {
         let y2 = e.clientY - y1;
 
         // console.log("x2", x2, "y2", y2);
-
-        const bounding = widget.getBoundingClientRect();
+				
+				// we must change the positioning of the widget relative to the wrapper's positioning
+				// this is because the widget itself will mess with the positioning when dragging for some unknown reason
+        const bounding = wrapper.getBoundingClientRect();
 
         // rather than reassigning the position to your cursor location, we are updating the position properties instead
         // through adding an subtracting x2 and y2. By doing so, the element doesn't teleporate to your cursor position
         // when you start dragging.
         widget.style.left = bounding.left + x2 + "px";
         widget.style.top = bounding.top + y2 + "px";
+				// must also update the wrapper's location in the same why since we're taking the boundClientRect of the wrpaper
+				wrapper.style.left = bounding.left + x2 + "px";
+        wrapper.style.top = bounding.top + y2 + "px";
         let data = {
           x2: x2,
           y2: y2,
@@ -70,7 +115,7 @@ const handleMouseDown = (e, widget) => {
       const handleMouseUp = () => {
         window.removeEventListener("mousemove", handleMouseMove);
         window.removeEventListener("mouseup", handleMouseUp);
-        widget.classList.remove("widgit-dragging");
+        // widget.classList.remove("widgit-dragging");
         widget.style.opacity = null;
         widget.style.animation = null;
       };
@@ -89,21 +134,36 @@ const cloneWidget = (widget) => {
   widgetClone.classList.add("box");
   widgetClone.setAttribute("clone", true);
   parentNode.append(widgetClone);
+	
+	// wrap clone inside div
+	const clonedParentNode = widgetClone.parentNode;
+	const wrapper = document.createElement("div");
+	wrapper.appendChild(widgetClone);
+	clonedParentNode.appendChild(wrapper)
 
-	widgetClone.addEventListener("mousedown", (e) => handleMouseDown(e, widgetClone));
+  widgetClone.addEventListener("mousedown", (e) =>
+    handleMouseDown(e, widgetClone)
+  );
 
   // reset widget timeout
   widgetClone.addEventListener("mouseup", () => resetTimeout(timeout_id));
   widgetClone.addEventListener("mouseleave", () => resetTimeout(timeout_id));
+	// widgetClone.addEventListener("click", () => widgetClone.remove())
+  const bounding = widget.getBoundingClientRect();
 
-	const bounding = widget.getBoundingClientRect();
-	widgetClone.style.position = "fixed";
-	widgetClone.style.top =  bounding.top + "px";
-	widgetClone.style.left = bounding.left + "px";
+	// must change wrapper's position in the same way since we'll be moving the element relative to the wrapper
+	// and not the widget itself
+  widgetClone.style.position = "fixed";
+	wrapper.style.position = "fixed";
+  widgetClone.style.top = bounding.top + "px";
+  widgetClone.style.left = bounding.left + "px";
+	wrapper.style.top = bounding.top + "px";
+  wrapper.style.left = bounding.left + "px";
   return widgetClone;
 };
 
 const handleResizer = (widget, resizeNodes) => {
+	const wrapper = widget.parentNode
   resizeNodes.forEach((node) => {
     const handleResizeMouseDown = (e) => {
       currentResizer = e.target;
@@ -117,22 +177,36 @@ const handleResizer = (widget, resizeNodes) => {
         const x2 = e.clientX - x1;
         const y2 = e.clientY - y1;
 
-        if (currentResizer.classList.contains("widgit-se")) {
+				// before we introduced the idea of positioning relative to a wrapper, resizing was weird
+				// now that we have a wrapper, it's good (must also update the width and height of the wrapper the same way as widget too)
+        if (currentResizer.getAttribute("se")) {
           widget.style.width = bounding.width + x2 + "px";
           widget.style.height = bounding.height + y2 + "px";
-        } else if (currentResizer.classList.contains("widgit-sw")) {
+					wrapper.style.width = bounding.width + x2 + "px";
+          wrapper.style.height = bounding.height + y2 + "px";
+        } else if (currentResizer.getAttribute("sw")) {
           widget.style.width = bounding.width - x2 + "px";
           widget.style.height = bounding.height + y2 + "px";
           widget.style.left = bounding.left + x2 + "px";
-        } else if (currentResizer.classList.contains("widgit-ne")) {
+					wrapper.style.width = bounding.width - x2 + "px";
+          wrapper.style.height = bounding.height + y2 + "px";
+          wrapper.style.left = bounding.left + x2 + "px";
+        } else if (currentResizer.getAttribute("ne")) {
           widget.style.width = bounding.width + x2 + "px";
           widget.style.height = bounding.height - y2 + "px";
           widget.style.top = bounding.top + y2 + "px";
+					wrapper.style.width = bounding.width + x2 + "px";
+          wrapper.style.height = bounding.height - y2 + "px";
+          wrapper.style.top = bounding.top + y2 + "px";
         } else {
           widget.style.width = bounding.width - x2 + "px";
           widget.style.height = bounding.height - y2 + "px";
           widget.style.top = bounding.top + y2 + "px";
           widget.style.left = bounding.left + x2 + "px";
+					wrapper.style.width = bounding.width - x2 + "px";
+          wrapper.style.height = bounding.height - y2 + "px";
+          wrapper.style.top = bounding.top + y2 + "px";
+          wrapper.style.left = bounding.left + x2 + "px";
         }
 
         x1 = e.clientX;
@@ -151,16 +225,22 @@ const handleResizer = (widget, resizeNodes) => {
     node.addEventListener("mousedown", handleResizeMouseDown);
   });
 };
+
 const addResizer = (widget) => {
   const nw = document.createElement("div");
   const ne = document.createElement("div");
   const sw = document.createElement("div");
   const se = document.createElement("div");
 
-  nw.className += "widgit-sizer widgit-nw";
-  ne.className += "widgit-sizer widgit-ne";
-  sw.className += "widgit-sizer widgit-sw";
-  se.className += "widgit-sizer widgit-se";
+	nw.setAttribute("nw", true)
+	ne.setAttribute("ne", true)
+	sw.setAttribute("sw", true)
+	se.setAttribute("se", true)
+
+  nw.style.cssText += resizerStyle_nw + resizerStyle;
+  ne.style.cssText += resizerStyle_ne + resizerStyle;
+  sw.style.cssText += resizerStyle_sw + resizerStyle;
+  se.style.cssText += resizerStyle_se + resizerStyle;
 
   widget.append(nw, ne, sw, se);
   handleResizer(widget, [nw, ne, sw, se]);
